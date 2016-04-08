8cee98429a0fcdd92f8e1846ac00eb9a91d274f1
Saint <Saint@192-164-0-237.adsl.highway.telekom.at>
2016-04-08 16:45:14 +0800

    test

---
+++ quiz.js
@@ -0,0 +1,181 @@
+﻿(function($) {
+    $.fn.jquizzy = function(settings) {
+        var defaults = {
+            questions: null,
+            startImg: 'images/start.png',
+            endText: '已结束!',
+            shortURL: null,
+            sendResultsURL: null,
+            resultComments: {
+                perfect: '文化大师说的就是你！',
+                excellent: '哎哟，不错哦！',
+                good: '哎哟，不错哦！',
+                average: '哎哟，不错哦！',
+                bad: '还需多多努力！',
+                poor: '你对中华传统文化的了解程度实在令人捉急。',
+                worst: '你对中华传统文化的了解程度实在令人捉急。'
+            }
+        };
+        var config = $.extend(defaults, settings);
+        if (config.questions === null) {
+            $(this).html('<div class="intro-container slide-container"><h2 class="qTitle">Failed to parse questions.</h2></div>');
+            return;
+        }
+        var superContainer = $(this),
+        answers = [],
+        introFob = '<div class="intro-container slide-container"><div class="nav-start" href="#"></div></div>	',
+        exitFob = '<div class="-container slide-container"><div class="question-number">' + config.endText + '</div><div class="result-keeper"></div></div><div class="notice">请选择一个选项！</div>',
+        contentFob = '',
+        questionsIteratorIndex,
+        answersIteratorIndex;
+        superContainer.addClass('main-quiz-holder');
+        for (questionsIteratorIndex = 0; questionsIteratorIndex < config.questions.length; questionsIteratorIndex++) {
+            contentFob += '<div class="slide-container"><div class="question-number">' + (questionsIteratorIndex + 1) + '/' + config.questions.length + '</div><div class="question">' + config.questions[questionsIteratorIndex].question + '</div><ul class="answers">';
+            for (answersIteratorIndex = 0; answersIteratorIndex < config.questions[questionsIteratorIndex].answers.length; answersIteratorIndex++) {
+                contentFob += '<li>' + config.questions[questionsIteratorIndex].answers[answersIteratorIndex] + '</li>';
+            }
+            contentFob += '</ul><div class="nav-container">';
+            if (questionsIteratorIndex !== 0) {
+                contentFob += '<div class="prev"><a class="nav-previous" href="#">&lt; 上一题</a></div>';
+            }
+            if (questionsIteratorIndex < config.questions.length - 1) {
+                contentFob += '<div class="next"><a class="nav-next" href="#">下一题 &gt;</a></div>';
+            } else {
+                contentFob += '<div class="next final"><a class="nav-show-result" href="#">完成</a></div>';
+            }
+            contentFob += '</div></div>';
+            answers.push(config.questions[questionsIteratorIndex].correctAnswer);
+        }
+        superContainer.html(introFob + contentFob + exitFob);
+        notice = superContainer.find('.notice'),
+        userAnswers = [],
+        questionLength = config.questions.length,
+        slidesList = superContainer.find('.slide-container');
+        function checkAnswers() {
+            var resultArr = [],
+            flag = false;
+            for (i = 0; i < answers.length; i++) {
+                if (answers[i] == userAnswers[i]) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+                resultArr.push(flag);
+            }
+            return resultArr;
+        }
+        function roundReloaded(num, dec) {
+            var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+            return result;
+        }
+        function judgeSkills(score) {
+            var returnString;
+            if (score === 100) return config.resultComments.perfect;
+            else if (score > 90) return config.resultComments.excellent;
+            else if (score > 70) return config.resultComments.good;
+            else if (score > 50) return config.resultComments.average;
+            else if (score > 35) return config.resultComments.bad;
+            else if (score > 20) return config.resultComments.poor;
+            else return config.resultComments.worst;
+        }
+        notice.hide();
+        slidesList.hide().first().fadeIn(500);
+        superContainer.find('li').click(function() {
+            var thisLi = $(this);
+            if (thisLi.hasClass('selected')) {
+                thisLi.removeClass('selected');
+            } else {
+                thisLi.parents('.answers').children('li').removeClass('selected');
+                thisLi.addClass('selected');
+            }
+        });
+        superContainer.find('.nav-start').click(function() {
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).next().fadeIn(500);
+            });
+            return false;
+        });
+        superContainer.find('.next').click(function() {
+            if ($(this).parents('.slide-container').find('li.selected').length === 0) {
+                notice.fadeIn(300);
+                return false;
+            }
+            notice.hide();
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).next().fadeIn(500);
+            });
+            return false;
+        });
+        superContainer.find('.prev').click(function() {
+            notice.hide();
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).prev().fadeIn(500);
+            });
+            return false;
+        });
+        superContainer.find('.final').click(function() {
+            if ($(this).parents('.slide-container').find('li.selected').length === 0) {
+                notice.fadeIn(300);
+                return false;
+            }
+            superContainer.find('li.selected').each(function(index) {
+                userAnswers.push($(this).parents('.answers').children('li').index($(this).parents('.answers').find('li.selected')) + 1);
+            });
+            if (config.sendResultsURL !== null) {
+                var collate = [];
+                for (r = 0; r < userAnswers.length; r++) {
+                    collate.push('{"questionNumber":"' + parseInt(r + 1, 10) + '", "userAnswer":"' + userAnswers[r] + '"}');
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: config.sendResultsURL,
+                    data: '{"answers": [' + collate.join(",") + ']}',
+                    complete: function() {
+                        console.log("OH HAI");
+                    }
+                });
+            }
+            var results = checkAnswers(),
+            resultSet = '',
+            trueCount = 0,
+            shareButton = '',
+            score,
+            url;
+            if (config.shortURL === null) {
+                config.shortURL = window.location
+            };
+            for (var i = 0,
+            toLoopTill = results.length; i < toLoopTill; i++) {
+                if (results[i] === true) {
+                    trueCount++;
+                    isCorrect = true;
+                }
+                resultSet += '<div class="result-row">' + (results[i] === true ? "<div class='correct'>#"+(i + 1)+"<span></span></div>": "<div class='wrong'>#"+(i + 1)+"<span></span></div>");
+            }
+            score = roundReloaded(trueCount / questionLength * 100, 2);
+            
+            resultSet = '<h2 class="qTitle">' + judgeSkills(score) + '<br/> 您的分数： ' + score + '</h2>' + 
+                        shareButton + '<div class="jquizzy-clear"></div>' +
+                        resultSet + '<div class="jquizzy-clear"></div>'  + '<div class="quit"></div>' + '<div class="jquizzy-clear"></div>';
+            superContainer.find('.result-keeper').html(resultSet).show(500);
+            
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).next().fadeIn(500);
+            });
+
+            superContainer.find('.quit').click(function() {
+                $(this).parents('.slide-container').fadeOut(500,
+                function() {
+                    $('.intro-container').fadeIn(500);
+                });
+                location.reload();
+            });
+
+            return false;
+        });
+    };
+})(jQuery);
